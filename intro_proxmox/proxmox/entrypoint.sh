df943df5f4809d12331e89fd8ffeaad1e1d6fc9b
Javier Fernández Peón <javierfp@iessanclemente.net>
2020-10-11 01:19:08 +0200

    proxmox despliegue

--- intro_proxmox/proxmox/entrypoint.sh
+++ intro_proxmox/proxmox/entrypoint.sh
@@ -1,13 +1,9 @@
 #!/bin/bash
 
 [ -d /etc/network ] || mkdir -p /etc/network
-echo "auto eth0" >> /etc/network/interfaces
-echo "iface eth0 inet static" >> /etc/network/interfaces
-echo "address $IPADD" >> /etc/network/interfaces
-echo "netmask 255.255.255.0" >> /etc/network/interfaces
-echo "gateway 192.168.199.1" >> /etc/network/interfaces
+sed -i "s/IP_ADDR/$IPADD/" /etc/network/interfaces
 
-ifdown eth0 && ifup eth0
+mount -a
 
 #Uso exec para lanzar el proceso del container
 exec bash -c "/lib/systemd/systemd"
